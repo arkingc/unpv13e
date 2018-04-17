dad5d543a8eca61ae14f9d8dc5f9ca7b95130ce4
chenximing <243744327@qq.com>
2018-04-17 21:06:34 +0800

    注释

--- lib/readline.c
+++ lib/readline.c
@@ -1,21 +1,31 @@
 /***************************************************************
- * readline：从一个描述符读文本行，一次1个字节
+ * 读取一行文本行
+ *
+ * 相比于前一个版本更高效。因为使用my_read替代read，my_read一次可以读取
+ * 最多MAXLINE个字符到readline的内部缓存区中，如果内部缓冲区有数据，
+ * my_read直接从内部缓存区取数据，而不需要每取一个字符都调用一次read
+ *
+ * 使用静态变量实现跨相继函数调用的状态信息维护，其结果是这些函数变得不可
+ * 重入或者说非线程安全了
  ***************************************************************/
 
 /* include readline */
 #include	"unp.h"
 
-static int	read_cnt;				//读取的字节数
-static char	*read_ptr;				//
-static char	read_buf[MAXLINE];		//读缓冲区
+static int	read_cnt;				//readline内部缓存区中的(可用)字节数
+static char	*read_ptr;				//指向内部缓冲区中第一个可读字符
+static char	read_buf[MAXLINE];		//readline内部缓冲区
 
+//返回的字符保存在ptr指向的字符中
 static ssize_t
 my_read(int fd, char *ptr)
 {
-
+	//只有当readline内部缓冲区为空时，才会读取，否则直接使用缓冲区中的字符数据
 	if (read_cnt <= 0) {
 again:
+		//read每次最多读取MAXLINE个字节，存入readline内部缓冲区
 		if ( (read_cnt = read(fd, read_buf, sizeof(read_buf))) < 0) {
+			//发生EINTR错误表示read调用被一个捕获的信号中断
 			if (errno == EINTR)
 				goto again;
 			return(-1);
@@ -24,7 +34,9 @@ again:
 		read_ptr = read_buf;
 	}
 
+	//可用字节数减一（因为需要返回缓冲区第一个字节）
 	read_cnt--;
+	//将ptr指向的字符设为内部缓冲区中的第一个字符，更新read_ptr指针
 	*ptr = *read_ptr++;
 	return(1);
 }
@@ -37,6 +49,7 @@ readline(int fd, void *vptr, size_t maxlen)
 
 	ptr = vptr;
 	for (n = 1; n < maxlen; n++) {
+		//使用my_read替代read，因此更高效
 		if ( (rc = my_read(fd, &c)) == 1) {
 			*ptr++ = c;
 			if (c == '\n')
@@ -52,6 +65,11 @@ readline(int fd, void *vptr, size_t maxlen)
 	return(n);
 }
 
+
+/*
+ * readlinebuf这个新函数能够展露内部缓冲区的状态，便于调用者查看在当前文本行
+ * 之后是否收到了新的数据
+ */
 ssize_t
 readlinebuf(void **vptrptr)
 {
--- test/readline1.c
+++ test/readline1.c
@@ -1,21 +1,32 @@
+
+/***************************************************************
+ * 读取一行文本行
+ *
+ * 这个版本每读一个字节的数据就调用一次系统的read函数，非常低效
+ ***************************************************************/
+
 /* include readline */
 #include	"unp.h"
 
-/* PAINFULLY SLOW VERSION -- example only */
+/* PAINFULLY SLOW（极端的慢） VERSION -- example only */
+//vptr指向存放数据的缓冲区
+//maxlen：存放读取到的字节的缓存区最大长度
 ssize_t
 readline(int fd, void *vptr, size_t maxlen)
 {
-	ssize_t	n, rc;
-	char	c, *ptr;
+	ssize_t	n, rc;		//rc：每次调用read读取的字符数
+	char	c, *ptr;	//c：存放每次读取的字符
 
 	ptr = vptr;
+	//最多读取(maxlen-1)个字符（最后一个位置放字符串结束符）
 	for (n = 1; n < maxlen; n++) {
 again:
 		if ( (rc = read(fd, &c, 1)) == 1) {
 			*ptr++ = c;
+			//读到换行符时停止
 			if (c == '\n')
 				break;	/* newline is stored, like fgets() */
-		} else if (rc == 0) {
+		} else if (rc == 0) {//已经没有数据可读（EOF）
 			*ptr = 0;
 			return(n - 1);	/* EOF, n - 1 bytes were read */
 		} else {
@@ -26,7 +37,7 @@ again:
 	}
 
 	*ptr = 0;	/* null terminate like fgets() */
-	return(n);
+	return(n);	
 }
 /* end readline */
 
